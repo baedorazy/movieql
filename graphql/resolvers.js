e1fb66e6b06eedfdfed5c23111c92bc5b4603fe2
jnbae <jnbae@jasongroup.co.kr>
2021-07-16 18:29:48 +0900

    graphQLtype step 2

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,23 @@
+const baezzy = {
+	name : 'baezzy',
+	age: 26,
+	gender : 'female'
+}
+
+// 아래 코드와 동일하지만 shortcut
 const resolvers = {
 	Query: {
-		name: () => 'baezzy'
+		person: () => baezzy
 	}
-}
+};
+
+//
+// const resolvers = {
+// 	Query: {
+// 		person: function () {
+// 			return baezzy
+// 		}
+// 	}
+// };
 
 export default resolvers;
